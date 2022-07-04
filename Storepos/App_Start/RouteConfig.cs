173a16d83b8d81cbc73c2fd84e57680a9c96073a
Alghofur <awalludin.i.a.g@gmail.com>
2022-07-04 11:59:35 +0700

    - update feature datepicker - update default home to register

--- Storepos/App_Start/RouteConfig.cs
+++ Storepos/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Storepos
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
